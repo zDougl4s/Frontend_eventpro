57f97f59d1d976bb6863f30730b33a418fe2bca2
Douglas <douglas@dobuilding.co.uk>
2020-10-04 22:56:50 +0100

    time update

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -38,7 +38,7 @@ export default class TicketCard extends Component {
   <div className="row whiteTextOverride">
       <section className="date whiteTextOverride">
         <time datetime="23th feb whiteTextOverride">
-          <span class="whiteTextOverride">23</span><span class="whiteTextOverride">feb</span>
+          <span class="whiteTextOverride">10</span><span class="whiteTextOverride">DEC</span>
         </time>
       </section>
       <section className="card-cont whiteTextOverride">
@@ -47,8 +47,8 @@ export default class TicketCard extends Component {
         <div className="even-date whiteTextOverride">
          <i className="fa fa-calendar whiteTextOverride"></i>
          <time>
-           <span>wednesday 28 december 2014</span>
-           <span>08:55pm to 12:00 am</span>
+           <span>wednesday 28 december 2030 Pandemic is over</span>
+           <span>08:00pm to 12:00 am</span>
          </time>
         </div>
         <div className="even-info whiteTextOverride">
